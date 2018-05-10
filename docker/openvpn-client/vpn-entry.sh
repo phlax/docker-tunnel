2ce75ba016f1c23a0db35e8825473919d6e66744
Ryan Northey <ryan@synca.io>
2018-05-10 22:08:02 +0100

    Add docker recipe for openvpn-client

---
+++ docker/openvpn-client/vpn-entry.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+envsubst '$VPN_SERVER_HOST$VPN_SERVER_PORT$VPN_CLIENT_NAME' < /etc/openvpn/client.conf.template > /etc/openvpn/$VPN_CLIENT_NAME.conf
+echo "Added vpn client $VPN_CLIENT_NAME ($VPN_SERVER_HOST:$VPN_SERVER_PORT)"
+
+cat /etc/openvpn/$VPN_CLIENT_NAME.conf
+
+echo "Starting openvpn..."
+openvpn --config /etc/openvpn/$VPN_CLIENT_NAME.conf
