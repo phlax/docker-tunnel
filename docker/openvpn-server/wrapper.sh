4812cbf7259f45e0adbdb60a540546b5afa4fb0e
Ryan Northey <ryan@synca.io>
2018-05-14 10:19:17 +0100

    Add forward rules for any named services in server

--- docker/openvpn-server/wrapper.sh
+++ docker/openvpn-server/wrapper.sh
@@ -3,9 +3,29 @@
 echo "Starting OpenVPN server"
 echo "" > /etc/openvpn/dns-names
 
+echo $OPENVPN_DNS_NAMES
+
 for NAME in $OPENVPN_DNS_NAMES; do
+    RETRIES=0
     echo $NAME >> /etc/openvpn/dns-names
     echo "Added DNS forwarding name $NAME"
+    until [ ! -z "$(getent hosts $NAME | awk '{ print $1 }')" ] || [ $RETRIES -lt 25 ]; do
+        RETRIES=`expr $RETRIES + 1`
+        sleep .2;
+    done
+    ADDRESS=$(getent hosts $NAME | awk '{ print $1 }')
+    if [ -z "$ADDRESS" ]; then
+        echo "WARNING: cannot resolve route to $NAME, no routes have been added"
+    else
+        NAT_IFACE=$(ip -o route get $ADDRESS | awk '{ print $3 }')
+        echo "Adding nat masq for $ADDRESS via $NAT_IFACE"
+        iptables -t nat -C POSTROUTING -s 192.168.254.0/24 -o $NAT_IFACE -j MASQUERADE || {
+            iptables -t nat -A POSTROUTING -s 192.168.254.0/24 -o $NAT_IFACE -j MASQUERADE
+        }
+        iptables -t nat -C POSTROUTING -s 192.168.255.0/24 -o $NAT_IFACE -j MASQUERADE || {
+            iptables -t nat -A POSTROUTING -s 192.168.255.0/24 -o $NAT_IFACE -j MASQUERADE
+        }
+    fi
 done
 
 envsubst '$VPN_SERVER_NAME' < /etc/openvpn/server.conf.template > /etc/openvpn/openvpn.conf
