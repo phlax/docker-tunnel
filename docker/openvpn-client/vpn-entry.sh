940c24755869d3accf7c5c16356389d2f7224ccf
Ryan Northey <ryan@synca.io>
2020-04-23 08:43:45 +0100

    Update Docker recipes
    - switch to vanilla alpine
    - move management socket directory
    - tidy shell scripts
    - enable port forwarding in the client

--- docker/openvpn-client/vpn-entry.sh
+++ docker/openvpn-client/vpn-entry.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 
-envsubst '$VPN_SERVER_HOST$VPN_SERVER_PORT$VPN_CLIENT_NAME' < /etc/openvpn/client.conf.template > /etc/openvpn/$VPN_CLIENT_NAME.conf
-echo "Added vpn client $VPN_CLIENT_NAME ($VPN_SERVER_HOST:$VPN_SERVER_PORT)"
+add_control_socket () {
+    if [ ! -z "$OVPN_MANAGEMENT_SOCKET" ]; then
+	echo "Adding management socket on: $OVPN_MANAGEMENT_SOCKET"
+	echo "management $OVPN_MANAGEMENT_SOCKET" >> /etc/openvpn/openvpn.conf
+    fi
+}
 
-if [ ! -z "$OPENVPN_MANAGEMENT_SOCKET" ]; then
-    echo "Adding management socket on: $OPENVPN_MANAGEMENT_SOCKET"
-    echo "management $OPENVPN_MANAGEMENT_SOCKET" >> /etc/openvpn/$VPN_CLIENT_NAME.conf
-fi
+mangle_templates () {
+    envsubst '$VPN_SERVER_HOST $VPN_SERVER_PORT $VPN_CLIENT_NAME' < /etc/openvpn/client.conf.template > /etc/openvpn/$VPN_CLIENT_NAME.conf
+    echo "Mangling templates: $OVPN_PORT_FORWARDS"
+    envsubst '$OVPN_PORT_FORWARDS $OVPN_MANAGEMENT_SOCKET' < /etc/openvpn/ovpn_env.sh.template > /etc/openvpn/ovpn_env.sh
+}
 
+
+fix_key_permissions () {
+    chmod og-rwx /etc/openvpn/client/*key
+}
+
+
+mangle_templates && source "/etc/openvpn/ovpn_env.sh"
+add_control_socket
+fix_key_permissions
+
+# echo "Added vpn client $VPN_CLIENT_NAME ($VPN_SERVER_HOST:$VPN_SERVER_PORT)"
+# echo "#### CONFIG"
 cat /etc/openvpn/$VPN_CLIENT_NAME.conf
+# echo "#### ENDCONFIG"
+# route -n
 
-echo "Starting openvpn..."
 exec openvpn --config /etc/openvpn/$VPN_CLIENT_NAME.conf
--- docker/openvpn-server/wrapper.sh
+++ docker/openvpn-server/wrapper.sh
@@ -1,43 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Starting OpenVPN server"
-echo "" > /etc/openvpn/dns-names
 
-echo $OPENVPN_DNS_NAMES
-
-for NAME in $OPENVPN_DNS_NAMES; do
-    RETRIES=0
-    echo $NAME >> /etc/openvpn/dns-names
-    echo "Added DNS forwarding name $NAME"
-    until [ ! -z "$(getent hosts $NAME | awk '{ print $1 }')" ] || [ $RETRIES -lt 25 ]; do
-        RETRIES=`expr $RETRIES + 1`
-        sleep .2;
+setup_dns_names () {
+    local address name retries routes
+    for name in $OPENVPN_DNS_NAMES; do
+	retries=0
+	until [ ! -z "$(getent hosts $name | awk '{ print $1 }')" ] || [ $retries -lt 25 ]; do
+            retries=`expr $retries + 1`
+            sleep .2;
+	done
+	address=$(getent hosts $name | awk '{ print $1 }')
+	if [ -z "$address" ]; then
+            echo "WARNING: cannot resolve route to $name, no routes have been added"
+	    return
+	fi
+	echo "Added address for $name: $address"
+	echo $name >> /etc/openvpn/dns-names
     done
-    ADDRESS=$(getent hosts $NAME | awk '{ print $1 }')
-    if [ -z "$ADDRESS" ]; then
-        echo "WARNING: cannot resolve route to $NAME, no routes have been added"
-    else
-        NAT_IFACE=$(ip -o route get $ADDRESS | awk '{ print $3 }')
-        echo "Adding nat masq for $ADDRESS via $NAT_IFACE"
-        iptables -t nat -C POSTROUTING -s 192.168.254.0/24 -o $NAT_IFACE -j MASQUERADE || {
-            iptables -t nat -A POSTROUTING -s 192.168.254.0/24 -o $NAT_IFACE -j MASQUERADE
-        }
-        iptables -t nat -C POSTROUTING -s 192.168.255.0/24 -o $NAT_IFACE -j MASQUERADE || {
-            iptables -t nat -A POSTROUTING -s 192.168.255.0/24 -o $NAT_IFACE -j MASQUERADE
-        }
+}
+
+add_control_socket () {
+    if [ ! -z "$OVPN_MANAGEMENT_SOCKET" ]; then
+	echo "Adding management socket on: $OVPN_MANAGEMENT_SOCKET"
+	echo "management $OVPN_MANAGEMENT_SOCKET" >> /etc/openvpn/openvpn.conf
     fi
-done
+}
 
-envsubst '$VPN_SERVER_NAME' < /etc/openvpn/server.conf.template > /etc/openvpn/openvpn.conf
-echo "Added vpn server config for: $VPN_SERVER_NAME"
-cat /etc/openvpn/openvpn.conf
+mangle_templates () {
+    export OVPN_SERVER=${OVPN_SERVER:-192.168.255.0/24}
+    address=$(ipcalc -n $OVPN_SERVER | cut -d"=" -f2)
+    netmask=$(ipcalc -m $OVPN_SERVER | cut -d"=" -f2)
+    export SERVER="$address $netmask"
+    envsubst '$VPN_SERVER_NAME $SERVER' < /etc/openvpn/server.conf.template > /etc/openvpn/openvpn.conf
+    envsubst '$VPN_SERVER_NAME $OVPN_SERVER $OVPN_ROUTES $OVPN_MANAGEMENT_SOCKET' < /etc/openvpn/ovpn_env.sh.template > /etc/openvpn/ovpn_env.sh
+    echo "#### CONFIG"
+    cat /etc/openvpn/openvpn.conf
+    echo "#### ENDCONFIG"
+}
 
-envsubst '$VPN_SERVER_NAME' < /etc/openvpn/ovpn_env.sh.template > /etc/openvpn/ovpn_env.sh
-echo "Added vpn server env for: $VPN_SERVER_NAME"
+fix_key_permissions () {
+    chmod og-rwx /etc/openvpn/server/*key
+}
 
-if [ ! -z "$OPENVPN_MANAGEMENT_SOCKET" ]; then
-    echo "Adding management socket on: $OPENVPN_MANAGEMENT_SOCKET"
-    echo "management $OPENVPN_MANAGEMENT_SOCKET" >> /etc/openvpn/openvpn.conf
-fi
-
+# echo "Configuring OpenVPN server"
+mangle_templates && source "$OPENVPN/ovpn_env.sh"
+setup_dns_names "${@}"
+add_control_socket
+fix_key_permissions
+# echo "Starting OpenVPN server"
 exec ovpn_run
